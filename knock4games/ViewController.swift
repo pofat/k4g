a2fb43cd70a58a63be4df4c6ab9a73e975c68635
Pofat Tseng <tjazzter@gmail.com>
2017-05-06 10:54:51 +0800

    First commit

---
+++ knock4games/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  knock4games
+//
+//  Created by Pofat Diuit on 2017/5/1.
+//  Copyright © 2017年 Pofat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ knock4gamesTests/knock4gamesTests.swift
@@ -0,0 +1,36 @@
+//
+//  knock4gamesTests.swift
+//  knock4gamesTests
+//
+//  Created by Pofat Diuit on 2017/5/1.
+//  Copyright © 2017年 Pofat. All rights reserved.
+//
+
+import XCTest
+@testable import knock4games
+
+class knock4gamesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
